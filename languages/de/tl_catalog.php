82850f7bb1017e7c636f3964ccde907bad89f4a9
Alexander Naumov <me@alexandernaumov.de>
2017-12-05 13:46:29 +0100

    add labels

---
+++ languages/de/tl_catalog.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog']['catalog_verification_legend'] = 'Verifizierungs-Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_catalog']['useEntityVerification'] = [ 'Verifizierung aktivieren', 'Hier können Sie die Datensätze verifizieren lassen. Damit können zum Beipsiel die eingetragene E-Mail Adresse überprüfen.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['catalogVerificationCodeColumn'] = [ 'Verifizierungscode-Spalte', 'Bitte wählen Sie eine Spalte für den Verifizierungscode aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['catalogIsVerifiedColumn'] = [ 'Verifizierungsstatus-Spalte', 'Bitte wählen Sie eine Spalte für den Verifizierungsstatus aus.' ];
